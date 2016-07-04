4b4edfa9f7a6eb2a701f5203713a232864486b0c
林学超 <linxuc0284@cairenhui.com>
2016-07-04 09:14:33 +0800

    修改样式

--- source/javascript/jquery-ui.js
+++ source/javascript/jquery-ui.js
@@ -2120,7 +2120,7 @@ $.extend(Datepicker.prototype, {
 					"<select><option>01</option><option>02</option><option>03</option><option>04</option><option>05</option><option>06</option>" +
 					"<option>07</option><option>08</option><option>09</option><option>10</option><option>11</option><option>12</option><option>13</option>" +
 					"<option>14</option><option>15</option><option>16</option><option>17</option><option>18</option><option>19</option><option>20</option>" +
-					"<option>21</option><option>22</option><option>23</option><option>24</option></select><button class='btn btn-fill ui-datepicker-sure'data-handler='selectHour' data-event='click'>确定</button></div>";
+					"<option>21</option><option>22</option><option>23</option><option>24</option></select><span>时</span><button class='btn btn-fill ui-datepicker-sure'data-handler='selectHour' data-event='click'>确定</button></div>";
 				}
 				group += calender;
 			}
