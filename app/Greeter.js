b6b9659997dc363f8a0c865ae434d59e6dd8d6b9
yanglong0339 <yanglong0339@cairenhui.com>
2017-09-26 19:13:09 +0800

    vue

---
+++ app/Greeter.js
@@ -0,0 +1,8 @@
+/**
+ * Created by dell on 2017/9/26.
+ */
+module.exports = function() {
+    var greet = document.createElement('div');
+    greet.textContent = "Hi there and greetings!";
+    return greet;
+};
---
+++ app/main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by dell on 2017/9/26.
+ */
+const greeter = require('./Greeter.js');
+document.querySelector("#root").appendChild(greeter());
